90736aff8440b7102697da780302087c677065ea
Joel Thompson <joel.thompson@intercom.io>
2018-01-11 11:58:11 -0800

    button

--- hackday_button.ino
+++ hackday_button.ino
@@ -1,10 +1,28 @@
+
+int input = 0;
+int inPin = 6;
+int outPin = 7;
+
 void setup() {
   // put your setup code here, to run once:
 
   // lets do this shit
+
+  pinMode(inPin, INPUT);
+  pinMode(outPin, OUTPUT);
+//  digitalWrite(7, LOW);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+
+
+
+  
+  input = digitalRead(inPin);
+  digitalWrite(outPin, input);  
+  
+  
 }
